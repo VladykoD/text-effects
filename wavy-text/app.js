4536de7b8847f3986827edba1b52ec276bc768ef
VladykoD <VladykoD@gmail.com>
2021-01-03 22:19:14 +0300

    moving text and same background

--- wavy-text/app.js
+++ wavy-text/app.js
@@ -6,15 +6,16 @@ class Animation {
       this.textSpeed = -6;
       this.waveAmplitude = 10
       this.waveLength = 200;
-      this.waveSpeed = -3;
+      this.waveSpeed = -2;
       this.c = new Canvas();
       this.createTextData();
       this.setupProperties();
-      this.createColorSet();
-      this.replaceTextColor();
+     // this.createColorSet();
+     // this.replaceTextColor();
       this.animate()
    }
    setupProperties() {
+      this.gradientLayer = this.c.createGradient();
       this.textSlicesCount = this.textArea.w / this.textSliceWidth;
       this.twoPI = Math.PI * 2;
       this.textY = this.c.h / 2 - this.textArea.h / 2;
@@ -23,6 +24,8 @@ class Animation {
    }
    createTextData() {
       this.textArea = this.c.drawText(this.mainText, this.textSize, 200, 300);
+
+      this.addImageLayer();
       this.textData = this.c.gid(this.textArea);
       this.c.clear();
    }
@@ -75,10 +78,13 @@ class Animation {
 
       this.textX = (this.textX + this.textSpeed) % this.c.w;
    }
+   addImageLayer() {
+      this.c.gco(`source-atop`);
+      this.c.ctx.drawImage(this.c.createGradient(), 0, 0)
+   }
    animate() {
       this.c.clear();
       this.wave();
-
       requestAnimationFrame(() => this.animate());
    }
 }
@@ -109,6 +115,19 @@ class Canvas {
       const fw = this.ctx.measureText(t).width;
       return {x: x, y: y, w: fw, h: fs};
    }
+
+   createGradient() {
+      const gradient = this.ctx.createLinearGradient(0,0,this.w, this.h);
+      for (let i = 0; i < 360; i++) {
+         const p = i / 360;
+         gradient.addColorStop(p, `hsl(${i}, 100%, 50%)`)
+      }
+      this.rect(gradient, 0, 0, this.w, this.h)
+      const img = new Image();
+      img.src = this.cnv.toDataURL();
+      return img
+   }
+
    //get image data
    gid({x, y, w, h}) {
       return this.ctx.getImageData(x,y,w,h)
@@ -116,6 +135,10 @@ class Canvas {
    //put image data
    pid(d, x, y, dx, dy, dw, dh) {
       this.ctx.putImageData(d, x, y, dx, dy, dw, dh)
+   }
+   //global composite operation
+   gco(type) {
+      this.ctx.globalCompositeOperation = type
    }
 
    rect(c, x , y, w, h) {
