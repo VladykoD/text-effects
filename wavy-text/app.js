d873837a3767d569c2172a4b04d7c95290fae1c7
VladykoD <VladykoD@gmail.com>
2021-01-03 22:05:45 +0300

    move background and text at the same time

--- wavy-text/app.js
+++ wavy-text/app.js
@@ -1,15 +1,17 @@
 class Animation {
    constructor() {
       this.mainText = 'RAINBOW';
-      this.textSliceWidth = 20;
-      this.textSize = 200;
+      this.textSliceWidth = 1;
+      this.textSize = 260;
       this.textSpeed = -6;
-      this.waveAmplitude = 50
+      this.waveAmplitude = 10
       this.waveLength = 200;
-      this.waveSpeed = -1;
+      this.waveSpeed = -3;
       this.c = new Canvas();
       this.createTextData();
       this.setupProperties();
+      this.createColorSet();
+      this.replaceTextColor();
       this.animate()
    }
    setupProperties() {
@@ -23,6 +25,34 @@ class Animation {
       this.textArea = this.c.drawText(this.mainText, this.textSize, 200, 300);
       this.textData = this.c.gid(this.textArea);
       this.c.clear();
+   }
+
+   createColorSet() {
+      for (let i = 0; i < this.textArea.w; i++) {
+         const currentColor = `hsl(${i / this.textArea.w * 360}, 100%, 50%)`
+         this.c.rect(currentColor, i, 300, 1, 1);
+      }
+      this.colorSetData = this.c.gid({x: 0, y: 300, w: this.textArea.w, h: 1})
+   }
+
+   replaceTextColor() {
+      const colorSetPixelData = this.colorSetData.data;
+      const textPixelData = this.textData.data;
+
+      for (let x = 0; x < this.textData.width; x++) {
+         for (let y = 0; y < this.textData.height; y++) {
+            const red = (x + y * this.textData.width) * 4;
+            const green = red + 1;
+            const blue = red + 2;
+            const alpha = red + 3;
+
+            if (textPixelData[alpha] > 0) {
+               textPixelData[red] = colorSetPixelData[x * 4]
+               textPixelData[green] = colorSetPixelData[x * 4 + 1]
+               textPixelData[blue] = colorSetPixelData[x * 4 + 2]
+            }
+         }
+      }
    }
 
    wave() {
@@ -86,6 +116,11 @@ class Canvas {
    //put image data
    pid(d, x, y, dx, dy, dw, dh) {
       this.ctx.putImageData(d, x, y, dx, dy, dw, dh)
+   }
+
+   rect(c, x , y, w, h) {
+      this.ctx.fillStyle = c;
+      this.ctx.fillRect(x,y,w,h)
    }
 
    clear() {
