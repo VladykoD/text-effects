22e1211c5069425dd78582a23dfe029ccdaf028f
VladykoD <VladykoD@gmail.com>
2021-01-06 09:47:03 +0300

    letter-in-letter animation

--- kinetic-typography/letter-in-letter/visual.js
+++ kinetic-typography/letter-in-letter/visual.js
@@ -1,61 +1,47 @@
 import {Text} from "./text.js";
 import {Particle} from "./particle.js";
 
+export const RANDOM_TEXT = 'MRSAWESOME';
+
 export class Visual {
     constructor() {
         this.text = new Text();
 
-        this.texture = PIXI.Texture.from('particle.png');
+        this.textArr = RANDOM_TEXT.split('')
 
         this.particles = [];
 
         this.mouse = {
             x: 0,
             y: 0,
-            radius: 50
-        };
+            radius: 60
+        }
 
         document.addEventListener('pointermove', this.onMove.bind(this), false)
     }
 
-    show(stageWidth, stageHeight, stage) {
-        if (this.container) {
-            stage.removeChild(this.container);
-        }
+    show(stageWidth, stageHeight) {
+        const str = this.textArr[Math.round(Math.random() * (this.textArr.length - 1))]
+        this.pos = this.text.setText(str, 26, stageWidth, stageHeight);
 
-        this.pos = this.text.setText('Daria', 2, stageWidth, stageHeight);
-
-        this.container = new PIXI.ParticleContainer(
-            this.pos.length,
-            {
-                vertices: false,
-                position: true,
-                rotation: false,
-                scale: false,
-                uvs: false,
-                tint: true,
-            }
-        );
-
-        stage.addChild(this.container);
         this.particles = [];
         for (let i = 0; i < this.pos.length; i++) {
-            const item = new Particle(this.pos[i], this.texture);
-            this.container.addChild(item.sprite);
+            const item = new Particle(this.pos[i]);
             this.particles.push(item);
         }
     }
 
-    animate() {
+    animate(ctx, t) {
         for (let i = 0; i < this.particles.length; i++) {
             const item = this.particles[i];
+
             const dx = this.mouse.x - item.x;
             const dy = this.mouse.y - item.y;
             const dist = Math.sqrt(dx * dx + dy * dy);
             const minDist = item.radius + this.mouse.radius;
 
             if (dist < minDist) {
-                const angle = Math.atan2(dy,dx);
+                const angle = Math.atan2(dy, dx);
                 const tx = item.x + Math.cos(angle) * minDist;
                 const ty = item.y + Math.sin(angle) * minDist;
                 const ax = tx - this.mouse.x;
@@ -65,13 +51,13 @@ export class Visual {
                 item.collide();
             }
 
-            item.draw();
+            item.draw(ctx, t)
         }
     }
-
 
     onMove(e) {
         this.mouse.x = e.clientX
         this.mouse.y = e.clientY
     }
+
 }
