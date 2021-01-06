e4b3c54f3fb5eafb49be3f9f44c7170be406840e
VladykoD <VladykoD@gmail.com>
2021-01-06 06:15:39 +0300

    added kinetic animation with particles

--- kinetic-typography/particles/app.js
+++ kinetic-typography/particles/app.js
@@ -1,30 +1,54 @@
+import {Visual} from "./visual.js";
+
 class App {
    constructor() {
-      this.canvas = document.createElement('canvas');
-      document.body.appendChild(this.canvas);
-      this.ctx = this.canvas.getContext('2d');
+      this.setWebgl();
 
-      this.pixelRatio = window.devicePixelRatio > 1 ? 2 : 1;
+      WebFont.load({
+         google: {
+            families: ['Hind:700']
+         },
+         fontactive: () => {
+            this.visual = new Visual();
 
-      window.addEventListener('resize', this.resize.bind(this), false)
-      this.resize();
+            window.addEventListener('resize', this.resize.bind(this), false)
+            this.resize();
 
-      window.requestAnimationFrame(this.animate.bind(this));
+            requestAnimationFrame(this.animate.bind(this));
+         }
+      });
+   }
+
+   setWebgl() {
+      this.renderer = new PIXI.Renderer({
+         width: document.body.clientWidth,
+         height: document.body.clientHeight,
+         antialias: true,
+         transparent: false,
+         resolution: (window.devicePixelRatio > 1) ? 2 : 1,
+         autoDensity: true,
+         powerPreference: 'high-performance',
+         backgroundColor: 0xffffff,
+      })
+      document.body.appendChild(this.renderer.view)
+
+      this.stage = new PIXI.Container();
    }
 
    resize() {
       this.stageWidth = document.body.clientWidth;
       this.stageHeight = document.body.clientHeight;
 
-      this.canvas.width = this.stageWidth * this.pixelRatio;
-      this.canvas.height = this.stageHeight * this.pixelRatio;
-      this.ctx.scale(this.pixelRatio, this.pixelRatio);
+      this.renderer.resize(this.stageWidth, this.stageHeight)
+
+      this.visual.show(this.stageWidth, this.stageHeight, this.stage)
    }
 
-   animate() {
-      window.requestAnimationFrame(this.animate.bind(this))
+   animate(t) {
+      requestAnimationFrame(this.animate.bind(this));
 
-      this.ctx.clearRect(0,0,this.stageWidth, this.stageHeight)
+      this.visual.animate();
+      this.renderer.render(this.stage)
    }
 }
 
